2a788c47be4eac31cf7c9b99bc269ab33cdc6426
pratik mane <pratik.mane1299@gmail.com>
2021-05-19 00:01:35 +0530

    setup react-query

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,25 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+import styles from "../styles/Home.module.css";
+
+const queryClient = new QueryClient();
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Infinite Scroll</title>
-        <meta name="description" content="Infinite Scroll with react query" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <QueryClientProvider client={queryClient}>
+      <div className={styles.container}>
+        <Head>
+          <title>Infinite Scroll</title>
+          <meta name="description" content="Infinite Scroll with react query" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Infinite Scroll
-        </h1>
-      </main>
-    </div>
-  )
+        <main className={styles.main}>
+          <h1 className={styles.title}>Infinite Scroll</h1>
+        </main>
+      </div>
+    </QueryClientProvider>
+  );
 }
